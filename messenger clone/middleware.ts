e40ff1dad01612e9f1ffde7f440d2f00f0a54913
Satya Mythili Nuthalapati <80440277+SatyaMythili@users.noreply.github.com>
2024-07-16 18:50:48 -0700

    Add files via upload

---
+++ messenger clone/middleware.ts
@@ -0,0 +1,16 @@
+// You can use a Next.js Middleware with NextAuth.js to protect your site.
+
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+    pages: {
+        signIn: "/",
+    }
+});
+
+export const config = {
+    matcher:[
+        "/users/:path*",
+        "/conversations/:path*"
+    ]
+};
